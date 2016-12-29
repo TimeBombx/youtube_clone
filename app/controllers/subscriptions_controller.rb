7a646a4e22d72f5c8bfb08e8e6157259f34e7f3c
cvotta <cvotta@seczetta.com>
2016-12-29 15:55:27 -0500

    added most popular videos for non auth users

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,6 +1,10 @@
 class SubscriptionsController < ApplicationController
   def index
-    sub_ids = current_user.subscriptions.pluck(:sub_id)
-    @videos = Video.where(user_id: sub_ids)
+    if is_logged_in?
+      sub_ids = current_user.subscriptions.pluck(:sub_id)
+      @videos = Video.where(user_id: sub_ids)
+    else
+      @videos = Video.all.order(views: :desc).limit(10)
+    end
   end
 end
