3d43376ce6523a9283922eb847b8175286204f9c
TimeBombx <coreyvotta95@gmail.com>
2016-12-29 20:28:21 -0500

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+u = User.create(username: "Corey", email: "corey@videotube.com", password: "password", settings: {})
+u2 = User.create(username: "TimeBomb", email: "timebomb@videotube.com", password: "password", settings: {})
+
+v = Video.create(title: "Video title", video_loc: "test", description: "This is a video description", thumbnail: "test", views: 100, likes: 17, dislikes: 3, user_id: u.id)
+
+s = Subscription.create(user_id: u.id, sub_id: u2.id)
